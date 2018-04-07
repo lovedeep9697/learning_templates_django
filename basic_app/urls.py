419c7d71d3181c8ff53db464e43ee02fbdcb586a
lovedeep <lovedeepsingh9697@gmail.com>
2018-04-08 04:59:38 +0530

    first commit

---
+++ basic_app/urls.py
@@ -0,0 +1,13 @@
+from basic_app import views
+from django.conf.urls import url
+
+
+app_name = 'basic_app'
+
+urlpatterns = [
+
+	url(r'^other/$',views.other,name='other'),
+	url(r'^relative/$',views.relative,name ='relative ')
+
+
+]
